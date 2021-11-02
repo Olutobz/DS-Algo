57f00db05ab816f6e71f5c02bec75ee7eb4e849c
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-11-02 20:46:38 +0100

    add basic method implementations

--- src/datastructures/dynamicarray/DynamicArray.java
+++ src/datastructures/dynamicarray/DynamicArray.java
@@ -9,18 +9,55 @@ package datastructures.dynamicarray;
 
 public class DynamicArray<T> {
 
-    // The initial static array container
     private T[] arr;
-    // The length of the static array user thinks
-    private int len;
+    // The length user think the array is
+    private int len = 0;
     // The actual array size
     private int capacity = 0;
-
-    public DynamicArray(int capacity) {
-        this.capacity = capacity;
-    }
 
     public DynamicArray() {
         this(16);
     }
+
+    public DynamicArray(int capacity) {
+        if (capacity < 0)
+            throw new IllegalArgumentException("Illegal Capacity: " + capacity);
+        this.capacity = capacity;
+        arr = (T[]) new Object[capacity];
+    }
+
+    public int size() {
+        return len;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * This returns the value at this specific index
+     */
+    public T get(int index) {
+        if (index < 0 || index >= len)
+            throw new IndexOutOfBoundsException();
+        return arr[index];
+    }
+
+    /**
+     * This put an element at a specific location
+     */
+    public void set(int index, T elem) {
+        if (index < 0 || index >= len)
+            throw new IndexOutOfBoundsException();
+        arr[index] = elem;
+    }
+
+    public void clear() {
+        for (int i = 0; i < len; i++) {
+            arr[i] = null;
+        }
+        len = 0;
+    }
+
+
 }
