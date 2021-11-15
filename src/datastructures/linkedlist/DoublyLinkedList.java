91a8b5156b1b6bf2424e5294ac9fed0db426a6c3
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-11-15 23:14:49 +0100

    create doublyLinkedList, add node data properties

---
+++ src/datastructures/linkedlist/DoublyLinkedList.java
@@ -0,0 +1,35 @@
+package datastructures.linkedlist;
+
+public class DoublyLinkedList<T> {
+    private int size = 0;
+    private Node<T> head = null;
+    private Node<T> tail = null;
+
+    // Return the size of this linked list
+    public int size() {
+        return size;
+    }
+
+    // Is this linked list empty?
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    // Internal node class to represent data
+    private static class Node<T> {
+        private T data;
+        private Node<T> prev, next;
+
+        public Node(T data, Node<T> prev, Node<T> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return data.toString();
+        }
+    }
+
+}
